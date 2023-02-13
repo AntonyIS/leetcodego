f933a7983e222c3d1ccb0449040df083cc0709ca
AntonyIS <antonyshikubu@gmail.com>
2023-02-13 07:30:36 +0300

    Adding TwoSum challenge and solution

---
+++ leetcode001_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+
+	t.Run("TwoSum Test: 1", func(t *testing.T) {
+		nums := []int{2, 7, 11, 15}
+		expectedResult := []int{0, 1}
+		target := 9
+		results := TwoSum(nums, target)
+		if !reflect.DeepEqual(results, expectedResult) {
+			t.Errorf("Error: %d + %d != %d", results[0], results[1], target)
+		}
+	})
+
+	t.Run("TwoSum Test: 2", func(t *testing.T) {
+		nums := []int{3, 2, 4}
+		expectedResult := []int{1, 2}
+		target := 6
+		results := TwoSum(nums, target)
+		if !reflect.DeepEqual(results, expectedResult) {
+			t.Errorf("Error: %d + %d != %d", results[0], results[1], target)
+		}
+	})
+
+}
