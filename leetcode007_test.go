50c4c12997e1e09163eb9975dd821c9e56cc79eb
AntonyIS <antonyshikubu@gmail.com>
2023-02-16 16:44:26 +0300

    Adding the 'Find Min in rotated array' challenge and solution

---
+++ leetcode007_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func TestFindMin(t *testing.T) {
+	t.Run("Find Min test 1", func(t *testing.T) {
+		nums := []int{3, 4, 5, 1, 2}
+		expectedResult := 1
+		output := FindMin(nums)
+		if output != expectedResult {
+			t.Errorf("Error\nExpected %d\nOuput %d", expectedResult, output)
+		}
+	})
+	t.Run("Find Min test 2", func(t *testing.T) {
+		nums := []int{4, 5, 6, 7, 0, 1, 2}
+		expectedResult := 0
+		output := FindMin(nums)
+		if output != expectedResult {
+			t.Errorf("Error\nExpected %d\nOuput %d", expectedResult, output)
+		}
+	})
+	t.Run("Find Min test 3", func(t *testing.T) {
+		nums := []int{11, 13, 15, 17}
+		expectedResult := 11
+		output := FindMin(nums)
+		if output != expectedResult {
+			t.Errorf("Error\nExpected %d\nOuput %d", expectedResult, output)
+		}
+	})
+}
